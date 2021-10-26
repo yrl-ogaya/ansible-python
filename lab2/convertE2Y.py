9246f77b65c98ccae491b674a7699ce3b7ae44dc
to-yuki <to-yuki@jtp.co.jp>
2021-10-26 17:27:08 +0900

    pycode update

--- lab2/convertE2Y.py
+++ lab2/convertE2Y.py
@@ -1,4 +1,10 @@
 import openpyxl
 
-wb = openpyxl.load_workbook("AnsiblePythonLab.xlsx")
-print("s")
+# Excel ファイルを指定する
+wbook = openpyxl.load_workbook("lab2/AnsiblePythonLab.xlsx")
+
+# Excel ファイルのシートリストを取得する
+sheets = wbook.sheetnames
+
+
+print(sheets)
