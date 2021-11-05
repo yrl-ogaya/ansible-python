a1d7a6c2d28caccdcb361d7fecb7e80e6b069dfe
murakami12345 <mura1705@jtp.co.jp>
2021-11-05 16:25:12 +0900

    main

---
+++ lab3/src/test.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, abort, make_response ,request
+import subprocess
+# import json
+
+api = Flask(__name__)
+
+@api.route('/Users/test', methods=['GET'])
+def get():
+    search = request.args.get("search")
+    #email = request.form.get("search")
+    print(search)
+    result = {
+        "result":True,
+        "data":{
+            "user":"test"
+            }
+        }
+    subprocess.call("dir")
+    return make_response(jsonify(result))
+@api.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=3000)
