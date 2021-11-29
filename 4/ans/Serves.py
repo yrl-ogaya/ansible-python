689a3ed4d0590c9ae475b946d40c638de77cf111
murakami12345 <mura1705@jtp.co.jp>
2021-11-29 11:55:52 +0900

    1155

--- 4/ans/Serves.py
+++ 4/ans/Serves.py
@@ -11,9 +11,9 @@ api = Flask(__name__)
 def create():
     print('/createにアクセスがありました。')
     ip = request.args.get('ip')
-    subprocess.run(['python','convert.py',ip],shell=True)
+    subprocess.run(['python3','convert.py',ip],shell=True)
     print('yamlファイルが生成しました。')
-    return make_response('yamlファイルを作成しました。')
+    return make_response('yamlファイルを作成しました。\n')
 
 #ansibleの実行
 @api.route('/ansible', methods=['GET'])
@@ -21,7 +21,7 @@ def ansible():
     print('/ansibleにアクセスがありました。')
     #subprocess.run(['ansible','convert.py'],shell=True)
     print('ansibleを実行しました。')
-    return make_response('ansibleを実行しました。')
+    return make_response('ansibleを実行しました。\n')
 
 if __name__ == '__main__':
     serve(api, host='0.0.0.0', port=3000)
