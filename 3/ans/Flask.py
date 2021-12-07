4316ecffeab1b50afc4fd700f0a338394a4a931c
murakami12345 <mura1705@jtp.co.jp>
2021-12-07 12:53:08 +0900

    1253

--- 3/ans/Flask.py
+++ 3/ans/Flask.py
@@ -6,7 +6,7 @@ api = Flask(__name__)
 @api.route('/', methods=['GET'])
 def function():
      print("function")
-     return make_response('success')
+     return make_response('success\n')
  
 if __name__ == '__main__':
      serve(api, host='0.0.0.0', port=3000)
